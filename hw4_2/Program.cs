ceab8fe7185a7be621079a20b331e24c543fe308
Пользователь <@DESKTOP-6V93RUO>
2022-09-13 21:33:02 +0300

    Добавьте файлы проекта.

---
+++ hw4_2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Passenger p1 = new Passenger("Volvo", "XC90", 7000000, "Кроссовер", 5);
+            Cargo c1 = new Cargo("Hyundai", "HD78", 2900000, "Рефрижиратор", 5);
+            Moto m1 = new Moto("Kawasaki", "Z 1000SX", 1400000, "Спорт-туризм", 142);
+
+            Plane pl1 = new Plane("Zodiac", "CH 650", 3300000, 1, 925);
+            Helicopter h1 = new Helicopter("McDonnel-Douglas", "MD 900 Explorer", 75000000, 2, 1);
+            Airship a1 = new Airship("AY-30", "AY-30", 2500000, 2, "Гелий");
+
+            Ground[] gr = { p1, c1, m1 };
+            Air[] air = { pl1, h1, a1 };
+            TC[] tc = { p1, c1, m1, pl1, h1, a1 };
+            while (true)
+            {
+                WriteLine("Выберете действие, которое хотите произвести:\n " +
+                    "1. Распечатать весь автопарк; \n " +
+                    "2. Стоимость наземного транспорта; \n " +
+                    "3. Стоимость воздушного транспорта; \n " +
+                    "4. Стоимость всего автопарка; \n " +
+                    "5. Выйти из программы.");
+                WriteLine();
+
+                int posnum = Convert.ToInt32(ReadLine());
+
+                switch (posnum)
+                {
+                    case 1:
+                        {
+                            foreach (TC item in tc)
+                            {
+                                item.Work();
+                                item.Print();
+                                WriteLine();
+                            };
+                        }
+                        break;
+                    case 2:
+                        {
+                            WriteLine($"Стоимость всего наземного транспорта = {Sum(gr)}\n");
+                        }
+                        break;
+                    case 3:
+                        {
+                            WriteLine($"Стоимость всего воздушного транспорта = {Sum(air)}\n");
+                        }
+                        break;
+                    case 4:
+                        {
+                            WriteLine($"Стоимость всего парка ТС = {Sum(tc)}\n");
+                        }
+                        break;
+                    case 5:
+                        return;
+
+                    default:
+                        WriteLine("Вы выбрали несуществующий пункт меню. Выберите снова.");
+                        continue;
+                }
+            }
+        }
+
+        static int Sum(TC[] ar)
+        {
+            int sum = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i].Cost;
+            }
+            return sum;
+        }
+    }
+}
---
+++ hw4_2/TC.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw4_2
+{
+    abstract class TC
+    {
+        public string Stamp { get; set; } 
+        public string Model { get; set; } 
+        public int Cost { get; set; } 
+        public TC(string _Stamp, string _Model, int _Cost)
+        {
+            Stamp = _Stamp;
+            Model = _Model;
+            Cost = _Cost;
+        }
+
+        public virtual void Print()
+        {
+            Write($"\nМарка: {Stamp} Модель: {Model} Стоимость: {Cost} руб.");
+        }
+        public abstract void Work();
+    }
+
+    abstract class Ground:TC
+    {
+        public string Type { get; set; }
+        public Ground(string _Stamp, string _Model, int _Cost, string _Type):base(_Stamp, _Model, _Cost)
+        {
+            Type = _Type;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Write($" Тип: {Type}");
+        }
+        public override void Work()
+        {
+            Write("Наземный транспорт. ");
+        }
+    }
+
+    class Passenger: Ground
+    {
+        public int PassengerNumbers { get; set; }
+        public Passenger(string _Stamp, string _Model, int _Cost, string _Type, int _PassengerNumbers) : base(_Stamp, _Model, _Cost, _Type)
+        {
+            PassengerNumbers = _PassengerNumbers;
+        }
+        public override void Print()
+        {
+            base.Print();
+            WriteLine($" Количество пассажиров: {PassengerNumbers} чел.");
+        }
+        public override void Work()
+        {
+            base.Work();
+            Write("Легковой. ");
+        }
+    }
+
+    class Cargo : Ground
+    {
+        public int Capacity { get; set; }
+        public Cargo(string _Stamp, string _Model, int _Cost, string _Type, int _Capacity) : base(_Stamp, _Model, _Cost, _Type)
+        {
+            Capacity = _Capacity;
+        }
+        public override void Print()
+        {
+            base.Print();
+            WriteLine($" Грузоподъемность: {Capacity} т");
+        }
+        public override void Work()
+        {
+            base.Work();
+            Write("Грузовой. ");
+        }
+
+    }
+
+    class Moto: Ground
+    {
+        public int Power { get; set; }
+        public Moto(string _Stamp, string _Model, int _Cost, string _Type, int _Power) : base(_Stamp, _Model, _Cost, _Type)
+        {
+            Power = _Power;
+        }
+        public override void Print()
+        {
+            base.Print();
+            WriteLine($" Мощность: {Power} л.с.");
+        }
+        public override void Work()
+        {
+            base.Work();
+            Write("Мотоцикл. ");
+        }
+    }
+
+    abstract class Air : TC
+    {
+        public int NumberEngines { get; set; }
+        public Air(string _Stamp, string _Model, int _Cost, int _NumberEngines) : base(_Stamp, _Model, _Cost)
+        {
+            NumberEngines = _NumberEngines;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Write($" Вид: {NumberEngines}");
+        }
+        public override void Work()
+        {
+            Write("Воздушный транспорт. ");
+        }
+    }
+
+    class Plane: Air
+    {
+        public int Distance { get; set; }
+        public Plane(string _Stamp, string _Model, int _Cost, int _NumberEngines, int _Distance) : base(_Stamp, _Model, _Cost, _NumberEngines)
+        {
+            Distance = _Distance;
+        }
+        public override void Print()
+        {
+            base.Print();
+            WriteLine($" Дальность: {Distance} км");
+        }
+        public override void Work()
+        {
+            base.Work();
+            Write("Самолет. ");
+        }
+    }
+
+    class Helicopter : Air
+    {
+        public int NumberScrews { get; set; }
+        public Helicopter(string _Stamp, string _Model, int _Cost, int _NumberEngines, int _NumberScrews) : base(_Stamp, _Model, _Cost, _NumberEngines)
+        {
+            NumberScrews = _NumberScrews;
+        }
+        public override void Print()
+        {
+            base.Print();
+            WriteLine($" Количество несущих винтов: {NumberScrews}");
+        }
+        public override void Work()
+        {
+            base.Work();
+            Write("Вертолет. ");
+        }
+    }
+
+    class Airship : Air
+    {
+        public string FillingGas { get; set; }
+        public Airship(string _Stamp, string _Model, int _Cost, int _NumberEngines, string _FillingGas) : base(_Stamp, _Model, _Cost, _NumberEngines)
+        {
+            FillingGas = _FillingGas;
+        }
+        public override void Print()
+        {
+            base.Print();
+            WriteLine($" Заполняющий газ: {FillingGas}");
+        }
+        public override void Work()
+        {
+            base.Work();
+            Write("Дирижабль. ");
+        }
+    }
+
+    
+}
